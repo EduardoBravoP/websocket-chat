90fc7c4f332a379ef1054b40718eb1151e07d41a
Eduardo Bravo <eduardobravopires@gmail.com>
2021-08-15 10:38:00 -0300

    add some services

--- src/websocket/ChatService.ts
+++ src/websocket/ChatService.ts
@@ -1,7 +1,10 @@
 import { container } from "tsyringe";
 import { io } from "../http";
+import { CreateChatRoomService } from "../services/CreateChatRoomService";
 import { CreateUserService } from "../services/CreateUserService";
 import { GetAllUsersService } from "../services/GetAllUsersService";
+import { GetChatRoomByUsersService } from "../services/GetChatRoomByUsersService";
+import { GetUserBySocketIdService } from "../services/GetUserBySocketIdService";
 
 io.on("connect", (socket) => {
   socket.on("start", async (data) => {
@@ -23,5 +26,30 @@ io.on("connect", (socket) => {
     const users = await getAllUsersService.execute();
 
     callback(users);
+  });
+
+  socket.on("start_chat", async (data, callback) => {
+    const createChatRoomService = container.resolve(CreateChatRoomService);
+    const getChatRoomByUsersService = container.resolve(
+      GetChatRoomByUsersService
+    );
+    const getUserBySocketIdService = container.resolve(
+      GetUserBySocketIdService
+    );
+
+    const userLogged = await getUserBySocketIdService.execute(socket.id);
+
+    let room = await getChatRoomByUsersService.execute([
+      data.idUser,
+      userLogged._id,
+    ]);
+
+    if (!room) {
+      room = await createChatRoomService.execute([data.idUser, userLogged._id]);
+    }
+
+    console.log(room);
+
+    callback(room);
   });
 });
