c61a82f529e515c357597da6bcae5fa7f6e0991d
Eduardo Bravo <eduardobravopires@gmail.com>
2021-08-15 10:37:50 -0300

    created start chat event

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,4 +1,5 @@
 const socket = io("http://localhost:3333");
+let roomId = "";
 
 function onLoad() {
   const urlParams = new URLSearchParams(window.location.search)
@@ -53,6 +54,17 @@ function addUser(user) {
     </li>
   `
 }
+
+document.getElementById('users_list').addEventListener("click", (event) => {
+  if (event.target && event.target.matches("li.user_name_list")) {
+    const idUser = event.target.getAttribute("idUser")
+
+    socket.emit("start_chat", { idUser }, (data) => {
+
+      roomId = data.idChatRoom
+    })
+  }
+})
 
 onLoad()
 
